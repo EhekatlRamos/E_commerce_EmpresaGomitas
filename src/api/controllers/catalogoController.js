2c5d375f1f36e9751cb3799999fbdbdb57a0beb6
Ehekatl Valentino <ehekatl.ramos@gmail.com>
2025-10-31 00:00:16 -0600

    Realice la comptabilidad con la tabla de venta

--- src/api/controllers/catalogoController.js
+++ src/api/controllers/catalogoController.js
@@ -9,3 +9,21 @@ export const obtenerProducto = (req,res)=>{
         res.json(results);
     });
 };
+export const crearVentas = (req, res) => {
+    const { total } = req.body; // datos que vienen del frontend
+
+    if (!total) {
+        return res.status(400).json({ error: 'Falta el total de la venta' });
+    }
+    const iva = total * 0.16; // calcula el IVA (ejemplo 16%)
+    const metodoPago = 'Paypal';
+    const clienteId = 1;
+    const sql='INSERT INTO ventas (Subtotal, Iva, Hora, Fecha, Metodo_P, Cliente) VALUES (?, ?, CURTIME(), CURDATE(), ?, ?)';
+    db.query(sql, [total], (err, result) => {
+        if (err) {
+            console.error('Error al crear venta', err);
+            return res.status(500).json({ error: 'Error al crear la venta' });
+        }
+        res.json({ message: 'Venta creada correctamente', idVenta: result.insertId });
+    });
+};
--- src/api/routes/catalogoRoutes.js
+++ src/api/routes/catalogoRoutes.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import { obtenerProducto } from '../controllers/catalogoController.js';
-const router=express.Router();
-//Endpoints
-router.get('/productos',obtenerProducto);
-export default router;
+    import express from 'express';
+    import { obtenerProducto, crearVentas } from '../controllers/catalogoController.js';
+    const router=express.Router();
+    //Endpoints
+    router.get('/productos',obtenerProducto);
+    router.post('/ventas',crearVentas);
+    export default router;
